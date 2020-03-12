e4678f802f38e3bc63d650f9d4b1753cf6c4724f
cbaddertwo <47836776+cbaddertwo@users.noreply.github.com>
2020-03-12 01:34:04 -0400

    liftUp()

---
+++ robot-2020/src/main/java/frc/robot/Lift.java
@@ -0,0 +1,38 @@
+package frc.robot;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMax.SoftLimitDirection;
+
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+public class Lift
+{
+
+    CANSparkMax m_backLift, m_frontLift;
+    SpeedControllerGroup m_Lift;
+    public Lift ()
+    {
+
+        m_frontLift = new CANSparkMax(RMap.CANLiftMotorFront, CANSparkMaxLowLevel.MotorType.kBrushless);
+        m_backLift = new CANSparkMax (RMap.CANLiftMotorBack, CANSparkMaxLowLevel.MotorType.kBrushless);
+
+        m_frontLift.setIdleMode(IdleMode.kBrake);
+        m_backLift.setIdleMode(IdleMode.kBrake);
+
+        m_frontLift.setSoftLimit(SoftLimitDirection.kReverse, 5);
+        m_backLift.setSoftLimit(SoftLimitDirection.kReverse, 5);
+
+        m_Lift = new SpeedControllerGroup(m_frontLift, m_backLift);
+        
+
+    }
+
+	public void LiftUp() {
+
+        m_Lift.set(RMap.liftSpeed);
+        
+	}
+
+}
+
